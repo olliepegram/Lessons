ab5bc57e3097af2bf0ba08fc05c4c390146f96c9
Ollie Pegram <ollie@ollies-mbp.ait-sydney.net.au>
2016-08-09 16:51:01 +1000

    phone update

--- car.rb
+++ car.rb
@@ -1,29 +1,33 @@
 class Car
-  def initialize(make, age, top_speed, gas_tank_size)
+  def initialize(make, age, top_speed, gas_tank_size, permilage)
     @make = make
     @age = age
     @top_speed = top_speed
     @gas_tank_size = gas_tank_size
+    @fuel = gas_tank_size
+    @permilage = permilage
   end
 
-  attr_accessor :make, :age, :top_speed, :gas_tank_size
+  attr_accessor :make, :age, :top_speed, :gas_tank_size, :fuel, :permilage
 
+  def depleteFuel(kms)
+    @fuel = @fuel - (kms * permilage)
+  end
 end
 
 class Driver
-  def initialize(car, age, contact)
+  def initialize(name, car, age)
+    @name = name
     @car = car
     @age = age
-    @contact = contact
   end
 
-attr_accessor :car, :age, :contact
+  attr_accessor :name, :car, :age
 
 end
 
+person = Driver.new("John", Car.new("Toyota", 1988, 220, 15000, 2), 7)
 
-
-car = Car.new("Toyota", 11, 220, 30000)
-
-puts car.make
-puts car.age
+puts person.car.gas_tank_size
+puts person.car.depleteFuel(20)
+puts person.car.fuel
